0a5c1856ae9c5c092b44ea289376a4a2c284f562
IanTranPersonal <vinhxtran1@gmail.com>
2025-04-08 11:17:45 +1000

    Initial Commit

---
+++ TechnicalTask/TechnicalTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTaskApp.swift
+//  TechnicalTask
+//
+//  Created by Vinh Tran on 8/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TechnicalTaskTests/TechnicalTaskTests.swift
@@ -0,0 +1,16 @@
+//
+//  TechnicalTaskTests.swift
+//  TechnicalTaskTests
+//
+//  Created by Vinh Tran on 8/4/2025.
+//
+
+import Testing
+
+struct TechnicalTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
